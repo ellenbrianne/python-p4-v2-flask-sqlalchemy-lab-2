d98dc92652dab347824c592922aaf87fe3b0f6c9
Ellen Bennett <bennette831@gmail.com>
2023-11-29 17:17:10 -0700

    add serialize_rules to all 3 models to avoid recursion errors

--- server/models.py
+++ server/models.py
@@ -11,11 +11,13 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 
-class Customer(db.Model):
+class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+
+    serialize_rules = ('-reviews.customer', )
 
     reviews = db.relationship('Review', back_populates='customer')
 
@@ -25,26 +27,29 @@ class Customer(db.Model):
         return f'<Customer {self.id}, {self.name}>'
 
 
-class Item(db.Model):
+class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
-    # has many customers through reviews
+    serialize_rules = ('-reviews.item', )
+
     reviews = db.relationship('Review', back_populates='item')
 
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
     
-class Review(db.Model):
+class Review(db.Model, SerializerMixin):
     __tablename__ = 'reviews'
 
     id = db.Column(db.Integer, primary_key=True)
     comment = db.Column(db.String)
     customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
     item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    serialize_rules = ('-customer.reviews', '-item.reviews')
 
     #belongs to a Customer
     customer = db.relationship('Customer', back_populates='reviews')
